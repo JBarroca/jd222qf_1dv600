6d93c5c3218089d78cb577786fcbce00d78f62bb
Jose Barroca <josebarroca@gmail.com>
2019-02-08 23:08:34 +0100

    fixing typo

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hagman under construction");
+    System.out.println("Hangman under construction");
   }
 
 }
