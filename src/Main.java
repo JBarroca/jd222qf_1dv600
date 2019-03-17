f2056a68b8922fa29939d43712b5ff1c56220a7e
JBarroca <josebarroca@gmail.com>
2019-03-17 00:36:55 +0000

    can read and write high scores, Map working fine.

--- src/Main.java
+++ src/Main.java
@@ -5,19 +5,19 @@ public class Main {
 
 	public static void main(String[] args) throws IOException {
 		
-		Score scr = new Score(250);
-		System.out.println(scr.getScore());
-		scr.onRightTry();
-		System.out.println(scr.getScore());
-		scr.onRightWordGuess();
-		System.out.println(scr.getScore());
+		Score scr = new Score(2);
+					
+		Score.registerHighScore("tobias", scr.getScore());
+		Score.registerHighScore("jaquim", 315);
+		Score.registerHighScore("akjh", 1091);
+		Score.registerHighScore("buuu", 1);
+		Score.registerHighScore("labu", 163);
+		Score.registerHighScore("lakshmi", 999);
 		
+		Score.readHighScoreFile(Score.getHighScoreFile());
 		
-		File scoreFile = scr.getHighScoreFile();
-	
 		System.out.println(scr.isHighScore());
 		
-		scr.registerHighScore("tobias", scr.getScore());
 		
 		/*
 		Hangman game = new Hangman();
--- src/Score.java
+++ src/Score.java
@@ -1,9 +1,17 @@
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Score {
@@ -42,15 +50,26 @@ public class Score {
 		}
 	}
 	
+	/**
+	 * Returns true if the current game score is on the top 5 highest scores.
+	 * @return	true if the current game score is on the top 5 highest scores
+	 * @throws IOException
+	 */
 	public boolean isHighScore() throws IOException {
 		File highScoreFile = getHighScoreFile();
 		ArrayList<Integer> previousScores = getScoresList(highScoreFile);
 		return isTop5Score(previousScores, this.score);
 	}
 	
-	public File getHighScoreFile() throws IOException {
+	/**
+	 * If a highscores file already exists, returns a File object referencing it.
+	 * If none is found, creates the file and returns the File object referencing it.
+	 * @return	File object referencing the high scores text file.
+	 * @throws IOException
+	 */
+	public static File getHighScoreFile() throws IOException {
 		File file = new File("resources/highscore.txt");
-		if (file.isFile()) {
+		if (file.exists()) {
 			return file;
 		} else {
 			file.createNewFile();
@@ -58,17 +77,41 @@ public class Score {
 		}
 	}
 	
+	/**
+	 * Returns a sorted ArrayList containing every previous score registered
+	 * @param scoreFile	File object referencing the highscores text file
+	 * @return	a sorted ArrayList containing every previous score	
+	 * @throws FileNotFoundException	if no high scores text file is found
+	 */
 	public ArrayList<Integer> getScoresList(File scoreFile) throws FileNotFoundException {
 		ArrayList<Integer> scoreList = new ArrayList<>();
+		
+		/*
 		Scanner scoreScanner = new Scanner(scoreFile);
 		while (scoreScanner.hasNextInt()) {
 			scoreList.add(scoreScanner.nextInt());
 		}
 		scoreScanner.close();
+		*/
+		
+		Map<String, Integer> scoresMap = readHighScoreFile(scoreFile);
+		for(Map.Entry<String, Integer> entry : scoresMap.entrySet()) {
+			scoreList.add(entry.getValue());
+		}
+		/*
 		Collections.sort(scoreList);
+		Collections.reverse(scoreList); //ascending order
+		*/
+		System.out.println("scoreList: " + scoreList.toString());
 		return scoreList;
 	}
 	
+	/**
+	 * Returns true if the current score is, at least, the 5th highest recorded.
+	 * @param scoreList	a sorted ArrayList contatining every previous score
+	 * @param score		the current game score
+	 * @return			true if <code>score</code> is in the top 5 highest scores
+	 */
 	public boolean isTop5Score(ArrayList<Integer> scoreList, int score) {
 		if (scoreList.size() < 5) {
 			return true;
@@ -77,10 +120,16 @@ public class Score {
 		}
 	}
 	
-	public void registerHighScore(String playerName, int score) {
-		try {
-			PrintWriter scoreWriter = new PrintWriter(getHighScoreFile());
-			scoreWriter.print(playerName);
+	/**
+	 * Enters the current score in the high scores file by writing the player's
+	 * name and current game score.
+	 * @param playerName	the Player's specified name
+	 * @param score			the score to be registered
+	 */
+	public static void registerHighScore(String playerName, int score) {
+		try {			
+			PrintWriter scoreWriter = new PrintWriter(new BufferedWriter(new FileWriter("resources/highscore.txt", true)));
+			scoreWriter.print(playerName + " ");
 			scoreWriter.println(score);
 			scoreWriter.close();
 		} catch (FileNotFoundException e) {
@@ -90,6 +139,63 @@ public class Score {
 		}
 	}
 	
+	/**
+	 * Creates and returns a Map containing every pair player:score recorded in
+	 * the high scores file, sorted according to scores.
+	 * @param highScoresFile	File object referencing the high scores text file
+	 * @return	a Map (player:score), sorted according to scores
+	 */
+	public static Map<String, Integer> readHighScoreFile(File highScoresFile) {
+		HashMap<String, Integer> scoresMap1 = new HashMap<String, Integer>();
+		
+		try {
+			Scanner scoresScanner = new Scanner(highScoresFile);
+			while(scoresScanner.hasNext()) {
+				scoresMap1.put(scoresScanner.next(), scoresScanner.nextInt());
+			}
+			scoresScanner.close();			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		// sorting the Map according to scores 
+		Map<String, Integer> scoresMapSorted = sortByScore(scoresMap1);
 	
+		// print the sorted map
+        for (Map.Entry<String, Integer> en : scoresMapSorted.entrySet()) { 
+            System.out.println("Key = " + en.getKey() +  
+                          ", Value = " + en.getValue()); 
+        } 
+		return scoresMapSorted;
+	}
+	
+    
+	/**
+	 * Sorts a map containing (player:score) pairs according to score
+	 * source: https://www.geeksforgeeks.org/sorting-a-hashmap-according-to-values/ 
+	 * @param hm	The hashmap to be sorted
+	 * @return		A sorted HashMap of players:scores
+	 */
+	public static HashMap<String, Integer> sortByScore(HashMap<String, Integer> hm) 
+    { 
+        // Create a list from elements of HashMap 
+        List<Map.Entry<String, Integer> > list = 
+               new LinkedList<Map.Entry<String, Integer> >(hm.entrySet()); 
+  
+        // Sort the list 
+        Collections.sort(list, new Comparator<Map.Entry<String, Integer> >() { 
+            public int compare(Map.Entry<String, Integer> o1,  
+                               Map.Entry<String, Integer> o2) 
+            { 
+                return (o2.getValue()).compareTo(o1.getValue()); 
+            } 
+        }); 
+          
+        // put data from sorted list to hashmap  
+        HashMap<String, Integer> temp = new LinkedHashMap<String, Integer>(); 
+        for (Map.Entry<String, Integer> aa : list) { 
+            temp.put(aa.getKey(), aa.getValue()); 
+        } 
+        return temp; 
+    } 
 	
 }
